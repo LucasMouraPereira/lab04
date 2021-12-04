d081a1414d26fa40e76ce47a884b36647ce1aa8e
Isabella Nicácio <ibella.nicacio@gmail.com>
2021-12-04 15:46:10 -0300

    ft: db in-memory

---
+++ implementacao/backend/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO user (email, password, type) VALUES ('test1@test.com', 'test1', 'company');
+INSERT INTO user (email, password, type) VALUES ('test2@test.com', 'test2', 'student');
+INSERT INTO user (email, password, type) VALUES ('test3@test.com', 'test3', 'company');
+INSERT INTO user (email, password, type) VALUES ('test4@test.com', 'test4', 'student');
+INSERT INTO user (email, password, type) VALUES ('test5@test.com', 'test5', 'company');
