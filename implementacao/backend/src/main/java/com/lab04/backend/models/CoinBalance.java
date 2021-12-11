b462e019e2218c787b277e55fa3d34a84b98784a
Isabella Nicácio <ibella.nicacio@gmail.com>
2021-12-11 19:54:18 -0300

    ft: Adding relationship

--- implementacao/backend/src/main/java/com/lab04/backend/models/CoinBalance.java
+++ implementacao/backend/src/main/java/com/lab04/backend/models/CoinBalance.java
@@ -3,7 +3,15 @@ package com.lab04.backend.models;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.GenerationType;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.OneToOne;
+import javax.persistence.CascadeType;
+import javax.persistence.JoinColumn;
 import java.util.Date;
 
 @Getter
@@ -21,6 +29,10 @@ public class CoinBalance {
 
     @Column(name = "balance_number", nullable = false)
     private Float balanceNumber;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
 
     public CoinBalance() {
     }
